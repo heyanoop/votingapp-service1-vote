a253f537f387f759b8a1f7ab30e8346e81f99e96
anoop <anoopsabu@live.com>
2025-03-24 15:16:29 +0530

    test push 77

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Ajay")
-option_b = os.getenv('OPTION_B', "Rony")
+option_a = os.getenv('OPTION_A', "Cat")
+option_b = os.getenv('OPTION_B', "Dog")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
